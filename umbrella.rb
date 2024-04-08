ec8a4bc6d97901493d1ebe37551f5a7121979ace
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-04-08 06:15:45 +0000

    added keys to github library

--- umbrella.rb
+++ umbrella.rb
@@ -1,8 +1,8 @@
 require "http"
 require "json"
 
-pirate_weather_api_key = ENV["42vbaM1ZCf1pa6EKObOULDnxB9JWVXEh"]
-gmaps_key = ENV["AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"]
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
+gmaps_key = ENV.fetch("GMAPS_KEY")
 
 hello_world = "Hello, world!"
 puts hello_world
