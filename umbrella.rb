19e8621d6d0b0a54cbb99a69279d089cebebf4fc
fpothuc <164956630+fpothuc@users.noreply.github.com>
2024-04-08 06:07:34 +0000

    added gmaps key

--- umbrella.rb
+++ umbrella.rb
@@ -2,7 +2,7 @@ require "http"
 require "json"
 
 pirate_weather_api_key = ENV["42vbaM1ZCf1pa6EKObOULDnxB9JWVXEh"]
-gmaps_key = ENV["GMAPS_KEY"]
+gmaps_key = ENV["AIzaSyDKz4Y3bvrTsWpPRNn9ab55OkmcwZxLOHI"]
 
 hello_world = "Hello, world!"
 puts hello_world
